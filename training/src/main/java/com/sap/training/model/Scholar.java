cdb6b1f2694f709400f46c376ded5a50f7401dce
Kaushiki Bhattacharya <127515450+kaushiki-bhattacharya-001@users.noreply.github.com>
2023-05-18 13:52:52 +0530

    Update Scholar.java

--- training/src/main/java/com/sap/training/model/Scholar.java
+++ training/src/main/java/com/sap/training/model/Scholar.java
@@ -16,8 +16,8 @@ public class Scholar {
     @Column(name = "batch", nullable = false)
     private String batch;
     
-//     @Column(name = "manager")
-//     private String manager;
+    @Column(name = "manager")
+    private String manager;
 
     public long getId() {
         return id;
