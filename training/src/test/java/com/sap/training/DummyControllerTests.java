697558a65f801ddeb97f053aa4a1d4d02c9dca8e
kaushiki.bhattacharya@sap.com <kaushiki.bhattacharya@sap.com>
2023-05-25 14:41:10 +0530

    updated TestFiles

--- training/src/test/java/com/sap/training/DummyControllerTests.java
+++ training/src/test/java/com/sap/training/DummyControllerTests.java
@@ -1,33 +1,33 @@
-// package com.sap.training;
+ package com.sap.training;
 
-// import com.sap.training.web.DummyController;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
+ import com.sap.training.web.DummyController;
+ import org.junit.jupiter.api.Test;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.boot.test.context.SpringBootTest;
 
-// import static org.assertj.core.api.Assertions.assertThat;
+ import static org.assertj.core.api.Assertions.assertThat;
 
-// @SpringBootTest
-// class DummyControllerTests {
+ @SpringBootTest
+ class DummyControllerTests {
 
-//     @Autowired
-//     private DummyController dummyController;
+     @Autowired
+     private DummyController dummyController;
 
-//     @Test
-//     void testGetHelloWorld() {
-//         assertThat(dummyController.getHelloWorld()).isEqualTo("Hello World");
-//     }
+     @Test
+     void testGetHelloWorld() {
+         assertThat(dummyController.getHelloWorld()).isEqualTo("Hello World");
+     }
 
-//     @Test
-//     void testGetHelloWorldWithParam() {
-//         String name = "In Java";
-//         assertThat(dummyController.getHelloWorldWithParam(name)).isEqualTo("Hello World, " + name);
-//     }
+     @Test
+     void testGetHelloWorldWithParam() {
+         String name = "In Java";
+         assertThat(dummyController.getHelloWorldWithParam(name)).isEqualTo("Hello World, " + name);
+     }
 
-//     @Test
-//     void testGetHelloWorldWithParams() {
-//         String param = "123";
-//         assertThat(dummyController.getHelloWorldWithParams(param)).isEqualTo("Hello World, Parameter Value: " + param);
-//     }
+     @Test
+     void testGetHelloWorldWithParams() {
+         String param = "123";
+         assertThat(dummyController.getHelloWorldWithParams(param)).isEqualTo("Hello World, Parameter Value: " + param);
+     }
 
-// }
+ }
